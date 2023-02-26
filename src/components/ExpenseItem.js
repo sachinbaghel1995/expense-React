bc7ae09e2345633d131dc48888c86eaa5adbe86e
Sachin Baghel <baghel.sachin1996@gmail.com>
2023-02-26 16:30:09 +0530

    useState

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,29 +1,26 @@
+// import React,{useState} from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from './Expensedate'
 import Card from './Card'
+import { useState } from 'react'
 const ExpenseItem=(props)=>{
+  const [title,setTitle]=useState(props.title)
+  const [amount,setAmount]=useState(props.amount)
     const clickHandler=()=>{
+        setTitle('UpdatedTitle')
         console.log('clicked')
+        setAmount("$"+100)
     }
-    const deleteHandler=(e)=>{
-        console.log('delete')
-        let itemList=document.getElementById('root')
-        // if (e.target.classList.contains('expenses')) {
-        // var li = document.getElementsByClassName('expenses')
-        var li=e.target.parentElement
-                itemList.remove(li)
-        // }
 
-    }
     return(
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
-            <div className="expense-item__description"><h2>{props.title}</h2>
-             <div className="expense-item__price">{props.amount}</div>
+            <div className="expense-item__description"><h2>{title}</h2>
+             <div className="expense-item__price">{amount}</div>
             <div>{props.LocationOfExpenditure}</div>
             {/* <div className='expense-date'>{props.date.toLocaleString()}</div> */}
             <button onClick={clickHandler}>ChangeTitle</button>
-            <button onClick={deleteHandler}>DeleteExpense</button>
+           <button onClick={clickHandler}>ChangeExpense</button>
             
             </div>
         </Card>
