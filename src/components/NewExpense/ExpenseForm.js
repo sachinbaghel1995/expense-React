fbefc3c022616fc3f63412289a84cd2cfab93e5b
Sachin Baghel <baghel.sachin1996@gmail.com>
2023-02-28 14:51:31 +0530

    add expense button and cancel button

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -42,6 +42,7 @@ const ExpenseForm=(props)=>{
                 <input type='date' min='2019-01-01' max='2023-01-30' onChange={DateChangeHandler} value={updatedDate}/>
            </div>
            <div className="new-expense__actions">
+           <button type = "button" onClick = {props.cancelHandler}>Cancel</button>
             <button type="submit">Add Expense</button>
         </div>
         </div>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,16 +1,29 @@
-import React from "react";
-import './NewExpense.css'
+import React, { useState } from "react";
+import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
-const NewExpense=(props)=>{
-    const saveExpenseDataHandler=(updatedExpenseData)=>{
-const expenseData={
-    ...updatedExpenseData,
-    id:Math.random().toString()
-}
-  props.onAddExpense(expenseData)
-    }
-    return <div className="new-expense">
-<ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+
+const NewExpense = (props) => {
+  const [click, setClick] = useState(false);
+
+  const onClickHandler = () => {
+    setClick(true);
+  };
+
+  const stopOnClcikHandler = () => {
+    setClick(false);
+  };
+  const saveExpenseDataHandler = (updatedExpenseData) => {
+    const expenseData = {
+      ...updatedExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+  return (
+    <div className="new-expense">
+        {!click && <button onClick={onClickHandler}>Add New Expense</button>}
+        {click && (<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} cancelHandler = {stopOnClcikHandler} />)}
     </div>
-}
-export default NewExpense
+  );
+};
+export default NewExpense;
