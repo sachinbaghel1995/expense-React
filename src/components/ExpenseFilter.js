1af87bce4a47a08ac74328fd55cb0aebcb9137b3
Sachin Baghel <baghel.sachin1996@gmail.com>
2023-02-28 12:26:40 +0530

    make filter workable

--- src/components/ExpenseFilter.js
+++ src/components/ExpenseFilter.js
@@ -19,6 +19,7 @@ export default function ExpenseFilter(props) {
           id="options"
           onChange={dropDownChangeHandler}
         >
+          <option value="2019">2019</option>
           <option value="2020">2020</option>
           <option value="2021">2021</option>
           <option value="2022">2022</option>
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,16 +4,21 @@ import './Expenses.css'
 import Card from "./Card";
 import ExpenseFilter from "./ExpenseFilter";
 const Expenses = (props) => {
-  const [filteredYear,setFilteredYear]=useState('2020')
+  const [filteredYear,setFilteredYear]=useState('2021')
 
   const filterChangedHandler=(selectedYear)=>{
 setFilteredYear(selectedYear)
   }
+  const filteredExpenses=props.items.filter(expense=>{
+    return expense.date.getFullYear().toString()===filteredYear
+  })
+    
+  
   return (
     <div>
     <Card className="expenses" id="expenss">
-      <ExpenseFilter selected={filteredYear} onChange={filterChangedHandler}/>
-      {props.items.map((expense)=>(<ExpenseItem 
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
+      {filteredExpenses.map((expense)=>(<ExpenseItem 
       key={expense.id}
       title={expense.title}
       amount={expense.amount}
